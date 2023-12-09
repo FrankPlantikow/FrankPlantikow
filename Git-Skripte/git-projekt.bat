89256253593690b7b8b9d309a4715adcf1034d0f
Frank Plantikow <135122391+FrankPlantikow@users.noreply.github.com>
2023-12-09 21:33:42 +0100

    Update git-projekt.bat

--- Git-Skripte/git-projekt.bat
+++ Git-Skripte/git-projekt.bat
@@ -15,5 +15,23 @@ goto :end
 
 :found
 echo Verzeichnis gefunden: %resultPath%
+set "base_directory=FrankPlantikow"
+
+rem Erstelle Hauptverzeichnis, wenn es nicht existiert
+if not exist "%base_directory%" mkdir "%base_directory%"
+
+rem Erstelle Unterverzeichnisse im Hauptverzeichnis
+for %%i in ("Meine GitHub-Projekt" "Meine-eBuecher" "Meine-Arduino") do (
+    if not exist "%base_directory%\%%i" mkdir "%base_directory%\%%i"
+
+    rem Wenn es sich um "Meine GitHub-Projekt" handelt, erstelle weitere Unterverzeichnisse
+    if "%%i"=="Meine GitHub-Projekt" (
+        for %%j in ("BauProjekte" "FrankPlantikow" "git-bsp" "Radageraet" "Raumklima" "Wetterstation") do (
+            if not exist "%base_directory%\%%i\%%j" mkdir "%base_directory%\%%i\%%j"
+        )
+    )
+)
+
+echo Verzeichnisse erfolgreich erstellt.
 
 :end
