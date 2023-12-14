1b2dcc5d15a5c1bcdc1b83fc2e661aadb23d48f2
FrankPlantikow <frank@plantikow.eu>
2023-12-14 09:26:35 +0100

    Automatischer Commit: Update und Hochladen

--- Git-Skripte/git-sys.bat
+++ Git-Skripte/git-sys.bat
@@ -6,49 +6,49 @@ echo *             (c) Frank Plantikow             *
 echo *                                             *
 echo ***********************************************
 echo.
-cd /d D:\Documents\Meine-eBücher
-echo Aktualisiere und lade Meine-eBücher hoch...
+cd /d D:\Documents\Meine-eBuecher
+echo Aktualisiere und lade Meine-eBuecher hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\BauProjekte
+cd /d D:\Documents\Meine-GitHub-Projekt\BauProjekte
 echo Aktualisiere und lade BauProjekte hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\FrankPlantikow
+cd /d D:\Documents\Meine-GitHub-Projekt\FrankPlantikow
 echo Aktualisiere und lade FrankPlantikow hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\git-bsp
+cd /d D:\Documents\Meine-GitHub-Projekt\git-bsp
 echo Aktualisiere und lade git-bsp hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\Radageraet
+cd /d D:\Documents\Meine-GitHub-Projekt\Radageraet
 echo Aktualisiere und lade Radageraet hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\Raumklima
+cd /d D:\Documents\Meine-GitHub-Projekt\Raumklima
 echo Aktualisiere und lade Raumklima hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
 git pull
 git push
 
-cd /d D:\Documents\Meine GitHub-Projekt\Wetterstation
+cd /d D:\Documents\Meine-GitHub-Projekt\Wetterstation
 echo Aktualisiere und lade Wetterstation hoch...
 git add .
 git commit -m "Automatischer Commit: Update und Hochladen"
